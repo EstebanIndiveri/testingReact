348f48c887006a556729cc23607547011f9cd872
Esteban Indiveri <esteban.indiveri@gmail.com>
2020-08-08 22:08:01 -0300

    test async

---
+++ counter-app/src/test/base/09-promesas.test.js
@@ -0,0 +1,22 @@
+import { getHeroeByIdAsync } from "../../base-pruebas/09-promesas";
+import heroes from "../../data/heroes";
+
+describe('Purebas con promesas', () => {
+    test('heroe async ', (done) => {
+        const id=1;
+        getHeroeByIdAsync(id)
+        .then(heroe=>{
+            expect(heroe).toBe(heroes[0]);
+            done();
+        })
+    });
+    
+    test('error', (done) => {
+        const id=10;
+        getHeroeByIdAsync(id).catch(err=>{
+            expect(err).toBe('No se pudo encontrar el héroe');
+            done();
+        })
+    })
+    
+})
